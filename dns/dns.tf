ddbe9ddb615b6516547f11ce6c978469ed784954
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-08-03 16:21:20 +0200

    remove vaultwarden test domain

--- dns/dns.tf
+++ dns/dns.tf
@@ -13,11 +13,3 @@ resource "cloudflare_record" "teleport" {
   type     = "CNAME"
   proxied  = false
 }
-
-resource "cloudflare_record" "vaultwarden" {
-  zone_id  = var.cloudflare_zone_id
-  name     = "vaultwarden.test.erpf.de"
-  value    = "jakob.nodes.zelos.k8s.erpf.de"
-  type     = "CNAME"
-  proxied  = false
-}
