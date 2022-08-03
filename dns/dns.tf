e879f5dcf45a5a0a803a51012dccf27887eb182c
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-08-03 20:12:38 +0200

    add anonaddy and mail dns

--- dns/dns.tf
+++ dns/dns.tf
@@ -3,12 +3,28 @@ resource "cloudflare_record" "kubesphere" {
   name     = "kubesphere.erpf.de"
   value    = "jakob.nodes.zelos.k8s.erpf.de"
   type     = "CNAME"
-  proxied  = false
+  proxied  = true
 }
 
 resource "cloudflare_record" "teleport" {
   zone_id  = var.cloudflare_zone_id
   name     = "teleport.erpf.de"
+  value    = "jakob.nodes.zelos.k8s.erpf.de"
+  type     = "CNAME"
+  proxied  = true
+}
+
+resource "cloudflare_record" "anonaddy" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "anonaddy.erpf.de"
+  value    = "jakob.nodes.zelos.k8s.erpf.de"
+  type     = "CNAME"
+  proxied  = true
+}
+
+resource "cloudflare_record" "mail" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "mail.erpf.de"
   value    = "jakob.nodes.zelos.k8s.erpf.de"
   type     = "CNAME"
   proxied  = false
