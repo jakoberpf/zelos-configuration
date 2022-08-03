b8827ec8027e5a29e5678784b0d726e39813edcd
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-08-03 20:13:23 +0200

    add mailu deployment

---
+++ deployments/mailu/deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+kubectl apply -k .
+helm upgrade --install mailu /Users/jakoberpf/Code/jakoberpf/kubernetes/charts/charts/mailu --namespace=mailu --values=values.yaml
