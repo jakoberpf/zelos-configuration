b7e4795dafac692a88c0f7e51cf75485e630220a
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-21 23:01:59 +0100

    update vault deploy script

--- deployments/vault/deploy.sh
+++ deployments/vault/deploy.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 kubectl apply -k .
 
-helm upgrade --install vault hashicorp/vault --namespace=vault # --values=values.yaml
+helm upgrade --install vault erpf/vault --namespace=vault --values=values.yaml --version=0.0.3
 
 # https://developer.hashicorp.com/vault/docs/configuration/storage/s3
 # https://github.com/hashicorp/vault-helm/blob/main/values.yaml
