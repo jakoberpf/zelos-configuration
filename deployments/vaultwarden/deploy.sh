197d5f87f999b0b8ead922ef68efcb8c054e5529
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-24 11:33:32 +0100

    up vaultwarden chart version

--- deployments/vaultwarden/deploy.sh
+++ deployments/vaultwarden/deploy.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 kubectl apply -k .
 
-helm upgrade --install vaultwarden erpf/vaultwarden --namespace=vaultwarden --version=0.1.12 --values=values.yaml
+helm upgrade --install vaultwarden erpf/vaultwarden --namespace=vaultwarden --version=0.1.13 --values=values.yaml
