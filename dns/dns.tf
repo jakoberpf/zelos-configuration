a05ad82ca4040a033e0ff32f16825ff27b78ba05
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-24 19:55:42 +0100

    add non ssl teleport dns and gatus for testing

--- dns/dns.tf
+++ dns/dns.tf
@@ -14,11 +14,27 @@ locals {
     "bashhub.erpf.de",
     "kubesphere.erpf.de",
     "netmaker.erpf.de",
-    "teleport.erpf.de",
     "anonaddy.erpf.de",
     "grafana.erpf.de",
     "vault.erpf.de",
     "mail.erpf.de",
     "iam.erpf.de"
+  ]
+}
+
+resource "cloudflare_record" "services_unproxied" {
+  for_each = toset(local.domains_unproxied)
+  zone_id  = var.cloudflare_zone_id
+  name     = each.key
+  value    = "jakob.nodes.zelos.k8s.erpf.de"
+  type     = "CNAME"
+  proxied  = false
+}
+
+locals {
+  domains_unproxied = [
+    "gatus.erpf.de",
+    "teleport.erpf.de",
+    "*.teleport.erpf.de",
   ]
 }
