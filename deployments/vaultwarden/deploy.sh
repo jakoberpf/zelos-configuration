f23e24e9b00c4e245b326418267fc1539224e7fd
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-12-23 23:00:00 +0800

    install vaultwarden with kustomize

--- deployments/vaultwarden/deploy.sh
+++ deployments/vaultwarden/deploy.sh
@@ -2,6 +2,17 @@
 
 set -eo pipefail
 
-kubectl apply -k .
+# view yaml manifest first
+kubectl kustomize --enable-helm
+kubectl kustomize --enable-helm | kubectl apply --dry-run=client -f -
 
-helm upgrade --install vaultwarden erpf/vaultwarden --namespace=vaultwarden --version=0.1.13 --values=values.yaml
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# apply to kubernetes cluster
+# 'apply' command does not have enable-helm flag
+kubectl kustomize --enable-helm | kubectl apply -f -
