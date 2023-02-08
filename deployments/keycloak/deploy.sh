30cebac25bfe4862274aef52000ed76a9212c9c2
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-02-08 08:34:53 +0100

    add postgres chart to keycloak and remove version from keycloak, aka rolling

--- deployments/keycloak/deploy.sh
+++ deployments/keycloak/deploy.sh
@@ -4,5 +4,8 @@ set -eo pipefail
 
 kubectl apply -k .
 
+# helm upgrade --install keycloak-postgres /Users/jakoberpf/Code/jakoberpf/kubernetes/charts/charts/postgres --namespace=keycloak --values=values.yaml
+# https://blog.knoldus.com/migrate-keycloak-h2-database-to-postgres-on-kubernetes/
+
 kubectl scale deploy -n keycloak --replicas=0 keycloak
-helm upgrade --install keycloak erpf/keycloak --namespace=keycloak --version=0.0.3 --values=values.yaml
+helm upgrade --install keycloak erpf/keycloak --namespace=keycloak --values=values.yaml
